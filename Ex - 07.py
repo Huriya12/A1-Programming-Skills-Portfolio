65f17c0f3b0edd424ae6f5791b9c3732cc0332e1
Huriya12 <164994418+Huriya12@users.noreply.github.com>
2024-10-24 20:58:51 +0400

    Add files via upload

---
+++ Ex - 07.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 18:00:39 2024
+
+@author: 97156
+"""
+"""
+Exercise 7: Loops
+"""
+#For loop that counts from 0 to 50 with increments of 1
+for a in range(0,51,1):
+    print(a) #Print results to the console
+
+#For loop that counts from 50 to 0 with decrements of 1
+for b in range(50,-1,-1):
+    print(b) #Print results to the console
+
+#For loop that counts from 30 to 50 with increments of 1
+for c in range(30,51,1):
+    print (c) #Print results to the console
+    
+#For loop that counts from 50 to 10 with decrements of 2
+for d in range(50,9,-2):
+    print(d) #Print results to the console
+
+#For loop that counts from 100 to 200 with increments of 5
+for e in range(100,201,5):
+    print(e) #Print results to the console

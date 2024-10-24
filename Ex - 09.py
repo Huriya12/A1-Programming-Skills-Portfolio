b498eb45058b6ebdb30966972224f4592379ebe5
Huriya12 <164994418+Huriya12@users.noreply.github.com>
2024-10-24 20:59:18 +0400

    Add files via upload

---
+++ Ex - 09.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 18:21:31 2024
+
+@author: 97156
+"""
+"""
+Exercise 9: Functions
+"""
+#Prints the word "Hello" to the console
+def hello():
+    print("Hello")
+
+#Calls for the hello() function    
+def main():
+    hello()
+    
+if __name__ == "__main__":
+    main()
+    

f909c7052c634d8cffd4585aba399d527379c790
Huriya12 <164994418+Huriya12@users.noreply.github.com>
2024-10-24 20:47:59 +0400

    Add files via upload

---
+++ EX - 1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 10:09:05 2024
+
+@author: 97156
+"""
+"""
+Exercise 1: Printing
+"""
+#Variable declaration for each word
+word1="Coding "
+word2="is "
+word3="Cool"
+
+#Print output to the console
+print(word1 + word2 + word3) #String concatenation to merge all variables
